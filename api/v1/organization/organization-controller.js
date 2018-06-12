f5ea362df6d15c7372f621f5c34d2bc39bb95258
Ridwan Maja <majataiwo@outlook.com>
2018-06-12 19:38:54 +0100

    finish add organization endpoint

--- api/v1/organization/organization-controller.js
+++ api/v1/organization/organization-controller.js
@@ -4,7 +4,7 @@ let db                  = require("../db.js"),
     _utils              = require("../utils.js");
 
 
-exports.addOrganization = function(req, res, next) {
+exports.addOrganizations = function(req, res, next) {
 
     db.conn.sync({force:false}).then(function() {
 
@@ -13,15 +13,21 @@ exports.addOrganization = function(req, res, next) {
             nodes               = serializedPayload.nodes,
             edges               = serializedPayload.edges;
 
+        parentModel.bulkCreate(edges).then(function() {
+            // nothing to do here...
+        }).catch(function(e){
+            req.errstatus = 500;
+            next(err)
+        })
+
         organizationModel.bulkCreate(nodes).then(function() {
 
-            // nothing to do here...
+            return organizationModel.findAll();
 
         }).then(function(nodes) {
-
             res.status(200).json(nodes);
 
-        }).catch((function(err) {
+        }).catch(function(err) {
             req.errstatus = 500;
             next(err)
         })
@@ -30,5 +36,10 @@ exports.addOrganization = function(req, res, next) {
         req.errstatus = 500;
         next(err)
     })
+}
+
+
+exports.getOrganizationRelations = function(req, res, next) {
+
 }
 
